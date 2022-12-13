fee495f0945ea03fc4f8aef335e5b43655364c61
mhcrocky <larastar721@gmail.com>
2022-12-13 15:58:39 +0400

    display team name instead of home/away---all

--- resources/views/themes/betting/partials/home/match.blade.php
+++ resources/views/themes/betting/partials/home/match.blade.php
@@ -13,51 +13,6 @@
 
     <div class="all-markets-questions" >
         <div class="row " v-for="(item, index) in allSports_filter" v-if="showType == 'live'">
-            <div class="col-lg-6" :key="question.id"  v-for="(question, index) in item.questions">
-                <div class="accordion mb-2"
-                     :id="'accordionExample_' + question.id">
-                    <div class="accordion-item">
-                        <h2 class="accordion-header" :id="'heading_' + question.id">
-                            <button
-                                class="accordion-button collapsed show"
-                                type="button"
-                                data-bs-toggle="collapse"
-                                :data-bs-target="'#collapse_' + question.id"
-                                aria-expanded="false"
-                                :aria-controls="'collapse_'+ question.id">
-                                @{{ question.name }}
-                            </button>
-                        </h2>
-                        <div   :id="'collapse_'+ question.id"
-                            class="accordion-collapse collapse"
-                            :aria-labelledby="'heading_'+ question.id"
-                            :data-bs-parent="'accordionExample_'+ question.id">
-                            <div class="accordion-body">
-                                <div class="row gy-1 ">
-                                    <div v-for="(option, index) in question.options" class="col-md-6">
-                                        <button @click="addToSlip(option)"
-                                            :disabled="option.is_unlock_question == 1 || option.is_unlock_match == 1" :class="{ disabled: (option.is_unlock_question == 1 || option.is_unlock_match == 1) }">
-                                            <i v-if="option.is_unlock_question == 1 || option.is_unlock_match == 1" class="fas fa-lock-alt mt-1"></i>
-                                          <span>
-                                             @{{ option.option_name }}
-                                          </span>
-                                            <span class="float-end">@{{ option.ratio}}</span>
-                                        </button>
-                                    </div>
-
-
-                                    <div  v-if="(question.options).length == 0" class="col-md-6">
-                                        <button class="disabled downgrade text-center" type="button">-</button>
-                                    </div>
-                                    <div  v-if="(question.options).length == 0" class="col-md-6">
-                                        <button class="disabled downgrade text-center" type="button">-</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
 
         <div class="row " v-for="(item, index) in upcoming_filter" v-if="showType == 'upcoming'">
@@ -89,9 +44,16 @@
                                                 <button @click="addToSlip(option)"
                                                     :disabled="option.is_unlock_question == 1 || option.is_unlock_match == 1" :class="{ disabled: (option.is_unlock_question == 1 || option.is_unlock_match == 1) }">
                                                     <i v-if="option.is_unlock_question == 1 || option.is_unlock_match == 1" class="fas fa-lock-alt mt-1"></i>
-                                                  <span>
-                                                     @{{ option.option_name }}
-                                                  </span>
+                                                  
+                                                    <span v-if="option.option_name == 'Home'">
+                                                        @{{item.team1}}
+                                                     </span>
+                                                     <span v-else-if="option.option_name == 'Away'">
+                                                        @{{item.team2}}
+                                                     </span>
+                                                     <span v-else>
+                                                        @{{ option.option_name }}
+                                                     </span>
                                                     <span class="float-end">@{{ option.ratio}}</span>
                                                 </button>
                                             </div>
@@ -140,9 +102,15 @@
                                                 <button @click="addToSlip(option)"
                                                     :disabled="option.is_unlock_question == 1 || option.is_unlock_match == 1" :class="{ disabled: (option.is_unlock_question == 1 || option.is_unlock_match == 1) }">
                                                     <i v-if="option.is_unlock_question == 1 || option.is_unlock_match == 1" class="fas fa-lock-alt mt-1"></i>
-                                                  <span>
-                                                     @{{ option.option_name }}
-                                                  </span>
+                                                    <span v-if="option.option_name == 'Home'">
+                                                        @{{item.team1}}
+                                                     </span>
+                                                     <span v-else-if="option.option_name == 'Away'">
+                                                        @{{item.team2}}
+                                                     </span>
+                                                     <span v-else>
+                                                        @{{ option.option_name }}
+                                                     </span>
                                                     <span class="float-end">@{{ option.ratio}}</span>
                                                 </button>
                                             </div>
